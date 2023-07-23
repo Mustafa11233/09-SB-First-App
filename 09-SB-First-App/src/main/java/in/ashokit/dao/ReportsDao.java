755f9be0d084bcccf915e975b79804a998b7d379
Mustafa11233 <140052516+Mustafa11233@users.noreply.github.com>
2023-07-23 19:28:59 +0530

    Add files via upload

---
+++ 09-SB-First-App/src/main/java/in/ashokit/dao/ReportsDao.java
@@ -0,0 +1,11 @@
+package in.ashokit.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReportsDao {
+	public ReportsDao() {
+		System.err.println("ReportsDa0 :: Constructor");
+	}
+
+}
